6738ac05c1d0dea8357cc49f940ca4ff7743620d
Rohit Gade <rohit14gade@gmail.com>
2025-01-08 10:23:45 +0530

    new updated'

--- src/main/java/com/krios/repository/DataRepository.java
+++ src/main/java/com/krios/repository/DataRepository.java
@@ -1,14 +1,11 @@
 package com.krios.repository;
 
-import java.awt.print.Pageable;
-
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.krios.entity.UserData;
 
 public interface DataRepository extends JpaRepository<UserData, Integer> {
 
-//	Page<UserData> findAll(Pageable pageable);
+
 
 }
